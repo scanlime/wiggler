91e6e4d1fb22706d8563ddd9527ed040b7b54040
Micah Elizabeth Scott <micah@misc.name>
2018-01-12 00:56:06 +0000

    Tweak ratio of inspiration to progress, less holes in the paper

--- bot.py
+++ bot.py
@@ -205,7 +205,7 @@ class GreatArtist:
         draw.line((s*from_pos[0], s*from_pos[1], s*to_pos[0], s*to_pos[1]), fill=255, width=2)
 
     def update_goal(self):
-        sub = ImageMath.eval("convert(1+a-b/2, 'L')", dict(a=self.inspiration, b=self.progress))
+        sub = ImageMath.eval("convert(1 + a - b + a/10, 'L')", dict(a=self.inspiration, b=self.progress))
         long_distance_blur = sub.filter(self.large_blur)
         self.goal = ImageMath.eval("convert(a+b, 'L')", dict(a=sub, b=long_distance_blur))
 
